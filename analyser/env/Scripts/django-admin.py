dbb9677e80e2f6b4bcb4856d07e36725b6246eb9
Nganga Anaene <ngangaanaene@gmail.com>
2020-07-25 21:55:55 +0100

    boilerplate for web analyser

---
+++ analyser/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lee\PycharmProjects\football-analyser\analyser\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
